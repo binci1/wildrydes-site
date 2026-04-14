f5f211159b8b9e466b837ecb5d7e34b7224dd179
unknown <tldhs0757@gmail.com>
2026-04-14 15:17:09 +0900

    v3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_JCUqPUuQl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7c099e6aobvurcjnd37enhb5nq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1v2jjihait44kb9ag8d1780k24', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
